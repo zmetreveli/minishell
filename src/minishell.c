df44d6faa55b0e20fbb6e27508d8e240a7a6112d
zurab <metreveli.zura.2014@gmail.com>
2025-04-04 09:46:52 +0200

    first_commitZ

---
+++ src/minishell.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zmetreve <zmetreve@student.42barcelon>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 09:06:27 by zurabmetrev       #+#    #+#             */
+/*   Updated: 2025/04/04 01:41:46 by zmetreve         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	minishell_noninteractive(t_data *data, char *arg)
+{
+	char	**user_inputs;
+	int		i;
+
+	user_inputs = ft_split(arg, ';');
+	if (!user_inputs)
+		exit_shell(data, EXIT_FAILURE);
+	i = 0;
+	while (user_inputs[i])
+	{
+		data->user_input = ft_strdup(user_inputs[i]);
+		if (parse_user_input(data) == true)
+			g_last_exit_code = execute(data);
+		else
+			g_last_exit_code = 1;
+		i++;
+		free_data(data, false);
+	}
+	free_str_tab(user_inputs);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	t_data	data;
+
+	ft_memset(&data, 0, sizeof(t_data));
+	if (!start_check(&data, ac, av) || !init_data(&data, env))
+		exit_shell(NULL, EXIT_FAILURE);
+	if (data.interactive)
+		minishell_interactive(&data);
+	else
+		minishell_noninteractive(&data, av[2]);
+	exit_shell(&data, g_last_exit_code);
+	return (0);
+}
