29f87defcdfdd3a843f2f21b4d184624cda07e0a
Zurab Metreveli <zmetreve@cbr4s4.42barcelona.com>
2025-04-05 01:47:12 +0200

    cd y echo

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zmetreve <zmetreve@student.42barcelon>     +#+  +:+       +#+        */
+/*   By: zmetreve <zmetreve@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 01:26:30 by zmetreve          #+#    #+#             */
-/*   Updated: 2025/04/04 01:28:00 by zmetreve         ###   ########.fr       */
+/*   Updated: 2025/04/05 00:32:23 by zmetreve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,5 @@
 #define MINISHELL_H
 
 int	main(int ac, char **av, char **env);
-
-
-
-
-
-
-
 
 #endif
