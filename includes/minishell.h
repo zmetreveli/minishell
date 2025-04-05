8a8252945badeaa53d351852e04d2e116b673ab6
Zurab Metreveli <zmetreve@cbr4s4.42barcelona.com>
2025-04-05 02:14:43 +0200

    include avance

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,8 +11,22 @@
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-int	main(int ac, char **av, char **env);
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <termcap.h>
+
+#include "libft.h"
 
 #endif
