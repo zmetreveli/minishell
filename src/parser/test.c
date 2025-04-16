8992c7e3a1bf2367dbb665a0bee30a761214a9f8
jbusomm <145897540+jbusomm@users.noreply.github.com>
2025-04-16 18:17:04 +0200

    Add test file to test readline and ENUM and start ideas for tokenization

---
+++ src/parser/test.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbusom-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 18:16:07 by jbusom-r          #+#    #+#             */
+/*   Updated: 2025/04/16 18:16:08 by jbusom-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+/*
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int	token;
+		enum test
+		{
+			True = 1,
+			False,
+			Three
+		};
+		if (strcmp("True", av[1]) == 0)
+			token = True;
+		else if (strcmp("False", av[1]) == 0)
+			token = False;
+		else if (strcmp("Three", av[1]) == 0)
+			token = Three;
+		else
+		{
+			printf("Wtf u on gang\n");
+			return (1);
+		}
+		if (token == True)
+			printf("True!\n");
+		if (token == False)
+			printf("False!\n");
+		if (token == Three)
+			printf("Three!\n");
+//		printf("%d %d %d", True, False, Three);
+	}
+	else
+	{
+		printf("Wrong argument count\n");	
+		return (1);
+	}
+}
+*/
+
+int	main(void)
+{
+		const char *prompt = ">>> ";
+		char *user_response = readline (prompt);
+		printf("My goat '%s' lets gooo\n", user_response);
+		free(user_response);
+		user_response = readline (prompt);
+		printf("command not found: %s\n", user_response);
+		free(user_response);
+		user_response = readline (prompt);
+		if (strcmp(user_response, "env") == 0)
+			printf("ENVIROMNETAL PRINTEADA\n");
+		else
+			printf("command not found: %s\n", user_response);
+		return 0;
+	//else
+	//	return (printf("wrong arguments dummy"), 1);
+}
