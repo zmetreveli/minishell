2b9b4b32a0173216bbdd2047f548cdeb5f06565b
jbusomm <145897540+jbusomm@users.noreply.github.com>
2025-04-22 14:22:09 +0200

    Update 22

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: zmetreve <zmetreve@student.42barcelona.    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/04 01:26:30 by zmetreve          #+#    #+#             */
-/*   Updated: 2025/04/05 00:32:23 by zmetreve         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -26,7 +14,18 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <termcap.h>
-
 #include "libft.h"
 
+typedef enum e_token_type
+{
+	WORD,
+	STRING,
+	PIPE,
+	QUOTE,
+	DQUOTE,
+	REDIR_IN,
+	REDIR_OUT,
+	APPEND,
+	HEREDOC
+}   t_token_type;
 #endif
