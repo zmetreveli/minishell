1e94e6bc4965e3af6e727b491786d49805421b5b
jbusomm <145897540+jbusomm@users.noreply.github.com>
2025-04-26 11:33:59 +0200

    Update env builtin and main file to print env

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: zmetreve <zmetreve@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 22:25:11 by zmetreve          #+#    #+#             */
-/*   Updated: 2025/04/25 16:22:43 by jbusom-r         ###   ########.fr       */
+/*   Updated: 2025/04/26 10:43:16 by jbusom-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,11 @@ int	env_builtin(t_data *data, char **args)
 	int	i;
 
 	if (args && args[1])
-		return (errmsg_cmd("env", NULL, "too many arguments", 2));
+		return (errmsg_cmd("env", NULL, "Too many arguments", 2));
 	i = 0;
 	if (!data->env)
 		return (EXIT_FAILURE);
 	while (data->env[i])
-		ft_putendl_fd(data->env[i++], STDOUT_FILENO);
+		printf("%s\n", data->env[i++]);
 	return (EXIT_SUCCESS);
 }
