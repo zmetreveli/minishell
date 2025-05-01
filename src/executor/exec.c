cb4ac8c29bab04f05e55692099843d82c8579299
zmetreve <metreveli.zura.2014@gmain.com>
2025-05-01 20:40:04 +0200

    execute

--- src/executor/exec.c
+++ src/executor/exec.c
@@ -3,10 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zmetreve <zmetreve@student.42barcelon>     +#+  +:+       +#+        */
+/*   By: zmetreve <zmetreve@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 01:31:14 by zmetreve          #+#    #+#             */
-/*   Updated: 2025/04/04 01:31:21 by zmetreve         ###   ########.fr       */
+/*   Updated: 2025/05/01 20:39:28 by zmetreve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	execute(t_data *data)
+{
+	int	ret;
+
+	ret = prep_for_exec(data);
+	if (ret != CMD_NOT_FOUND)
+		return (ret);
+	if (!data->cmd->pipe_output && !data->cmd->prev
+		&& check_infile_outfile(data->cmd->io_fds))
+	{
+		redirect_io(data->cmd->io_fds);
+		ret = execute_builtin(data, data->cmd);
+		restore_io(data->cmd->io_fds);
+	}
+	if (ret != CMD_NOT_FOUND)
+		return (ret);
+	return (create_children(data));
+}
